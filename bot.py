6b94cab12e035ee2ee48eda0fc18bf6526dcfbf9
Naoku <135733021+Naoku@users.noreply.github.com>
2023-12-21 23:22:13 +0100

    Update bot.py
    fixed.

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import requests, random
+num = 0
+randomParent = ''.join(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") for i in range(64))
 
 headers = {
     'authority': 'api.discord.gx.games',
     'accept': '*/*',
     'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
-    # Already added when you pass json=
-    # 'content-type': 'application/json',
     'origin': 'https://www.opera.com',
     'referer': 'https://www.opera.com/',
     'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
@@ -18,12 +18,16 @@ headers = {
 }
 
 json_data = {
-    'partnerUserId': "{parentuser}",
+    'partnerUserId': randomParent,
 }
+
 while True:
-    parentuser = ''.join(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") for i in range(64)) #stackoverflow gaming
-    link = "https://discord.com/billing/partner-promotions/1180231712274387115/" + requests.post('https://api.discord.gx.games/v1/direct-fulfillment', headers=headers, json=json_data).content.decode().removeprefix('{"token":"').removesuffix('"}') #gaming
-    file = open("fuckdiscord.txt", "a")
-    file.write(link+'\n')
-    file.close()
-    print(link)
+    randomParent = ''.join(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") for i in range(64))
+    discordCode = requests.post('https://api.discord.gx.games/v1/direct-fulfillment', headers=headers, json=json_data).content.decode().removeprefix('{"token":"').removesuffix('"}')
+    fullLink = "https://discord.com/billing/partner-promotions/1180231712274387115/" + discordCode
+    if discordCode != "":
+        file = open("fuckdiscord.txt", "a")
+        file.write(fullLink+'\n')
+        file.close()
+        num += 1
+        print(num, fullLink)
